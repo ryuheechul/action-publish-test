001072103340aa3f6663538a92a6f49a47004ecd
ryuheechul <ryuhcii@gmail.com>
2020-04-25 11:34:43 +0000

    deploy: f17f23fc930785399d6750405e89292cede87f94

--- _next/static/BO978EuuPdZ9H4rEn8Sgg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002F192c08574769cda4cc3b.css"]}}("static\u002Fchunks\u002Fcade47334a7fe28c61dfa50a8c015cf90df503fe.6e27b807322d50215a55.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
