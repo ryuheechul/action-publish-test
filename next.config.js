fb47f2538a9744c569e9ae0dc06ac3c65fdc0840
Heechul Ryu <ryuhcii@gmail.com>
2020-04-25 13:23:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const ghPages = !!process.env.NEXTJS_BACKEND_URL
 
 module.exports = {
   assetPrefix: ghPages ? process.env.NEXTJS_BACKEND_URL : '',
+  publicRuntimeConfig: {
+    basePath: ghPages ? process.env.NEXTJS_BACKEND_URL : ''
+  }
 }
