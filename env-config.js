0f888deeb620e499c20941941fe327073d4346e9
Heechul Ryu <ryuhcii@gmail.com>
2020-04-25 12:16:03 +0100

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const ghPages = !!process.env.NEXTJS_BACKEND_URL
 
 module.exports = {
-  'process.env.BACKEND_URL': ghPages ? ghPages : '',
+  'process.env.BACKEND_URL': ghPages ? process.env.NEXTJS_BACKEND_URL : '',
 }
